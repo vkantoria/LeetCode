d2edec162f73c82d70ac638117e77b179167b3e6
vkantoria <84955296+vkantoria@users.noreply.github.com>
2021-10-27 14:50:40 +0530

    Create 886. Possible Bipartition.cpp

---
+++ 886. Possible Bipartition.cpp
@@ -0,0 +1,75 @@
+/*
+We want to split a group of n people (labeled from 1 to n) into two groups of any size. Each person may dislike some other people, and they should not go into the same group.
+
+Given the integer n and the array dislikes where dislikes[i] = [ai, bi] indicates that the person labeled ai does not like the person labeled bi, return true if it is possible to split everyone into two groups in this way.
+
+ 
+
+Example 1:
+
+Input: n = 4, dislikes = [[1,2],[1,3],[2,4]]
+Output: true
+Explanation: group1 [1,4] and group2 [2,3].
+Example 2:
+
+Input: n = 3, dislikes = [[1,2],[1,3],[2,3]]
+Output: false
+Example 3:
+
+Input: n = 5, dislikes = [[1,2],[2,3],[3,4],[4,5],[1,5]]
+Output: false
+ 
+
+Constraints:
+
+1 <= n <= 2000
+0 <= dislikes.length <= 104
+dislikes[i].length == 2
+1 <= dislikes[i][j] <= n
+ai < bi
+All the pairs of dislikes are unique.
+
+*/
+class Solution {
+public:
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        
+        vector<int> color(n+1, -1);
+        // color.assign(n+1,-1);
+        vector<int> adj[n+1];
+        queue<int> q;
+    
+        for(auto v:dislikes)
+        {
+           adj[v[0]].push_back(v[1]); 
+           adj[v[1]].push_back(v[0]);  
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            if(color[i] == -1)
+        {     q.push(i);
+              color[i] = 0;
+            
+            
+        while(!q.empty())
+        {
+          int curr = q.front();
+            q.pop();
+            for(auto v:adj[curr])
+            {
+                if(color[v] == -1)
+                {
+                    color[v] = 1 - color[curr];
+                    q.push(v);
+                }
+                else if(color[v] == color[curr])
+                    return false;
+            }
+                    
+        }
+        }
+        }
+        return true;
+    }
+};
